749a4ce44f0d0ac446b05b077afaaeb3620c5434
ronastusha <ronastusha@gmail.com>
2025-05-20 20:26:35 +0300

    changes in files

--- labs/labs/lab_13/lab_13.cpp
+++ labs/labs/lab_13/lab_13.cpp
@@ -5,22 +5,23 @@ namespace lab_13
 {
 	class TBase
 	{
-		virtual std::string index() const = 0;
+		virtual std::string name() const = 0;
 	};
 
 	class TAminoAcid : public TBase
 	{
 	private:
-		std::string              FName;
-		char                     FCodon;
+		std::string FName;  // имя   (Глицин)
+		std::string FCodon; // кодон (GGU,GGC,GGA,GGG,)
+		char        FId;    // id    (G)
 	public:
-		TAminoAcid(const std::string& name, const char& codon) : FName(name), FCodon(codon) {}
-		std::vector<std::string> total;
-		std::string index() const override
+		TAminoAcid(const std::string& name, const std::string& codon, char id) : FName(name), FCodon(codon), FId(id) {}
+		//std::vector<std::string> total;
+		std::string name() const override
 		{
 			return FName;
 		}
-		char getcodon() const
+		std::string getCodon() const
 		{
 			return FCodon;
 		}
@@ -58,9 +59,58 @@ namespace lab_13
 			std::string str;
 			for (int i = 0; i < aminoAcids.size(); ++i)
 			{
-				str += aminoAcids[i].getcodon();
+				str += aminoAcids[i].getCodon();
 			}
 			return str;
+		}
+		std::string name() const
+		{
+			std::string str;
+			for (int i = 0; i < aminoAcids.size(); ++i)
+			{
+				str += aminoAcids[i].name();
+			}
+			return str;
+		}
+	};
+
+	class TTranslator
+	{
+	private:
+		std::vector<TAminoAcid> FAmins;
+	public:
+		TTranslator() : FAmins()
+		{
+			FAmins.push_back(TAminoAcid("Глицин", "GGU,GGC,GGA,GGG,", 'G'));
+			FAmins.push_back(TAminoAcid("Аланин", "GCU,GCC,GCA,GCG,", 'A'));
+			FAmins.push_back(TAminoAcid("Валин", "GUU,GUC,GUA,GUG,", 'V'));
+			FAmins.push_back(TAminoAcid("Изолейцин", "AUU,AUC,AUA,", 'I'));
+			FAmins.push_back(TAminoAcid("Лейцин", "UUA,UUG,CUU,CUC,CUA,CUG,", 'L'));
+			FAmins.push_back(TAminoAcid("Пролин", "CCU,CCC,CCA,CCG,", 'P'));
+			FAmins.push_back(TAminoAcid("Серин", "UCU,UCC,UCA,UCG,AGU,AGC", 'S'));
+			FAmins.push_back(TAminoAcid("Треонин", "ACU,ACC,ACA,ACG,", 'T'));
+			FAmins.push_back(TAminoAcid("Цистеин", "UGU,UGC,", 'C'));
+			FAmins.push_back(TAminoAcid("Местионин", "AUG,", 'M'));
+			FAmins.push_back(TAminoAcid("Аспарагиновая кислота", "GAU,GAC,", 'D'));
+			FAmins.push_back(TAminoAcid("Аспарагин", "AAU,AAC,", 'N'));
+			FAmins.push_back(TAminoAcid("Глутаминовая кислота", "GAA,GAG,", 'E'));
+			FAmins.push_back(TAminoAcid("Глутамин", "CAA,CAG,", 'Q'));
+			FAmins.push_back(TAminoAcid("Лизин", "AAA,AAG,", 'K'));
+			FAmins.push_back(TAminoAcid("Аргинин", "CGU,CGC,CGA,CGG,AGA,AGG,", 'R'));
+			FAmins.push_back(TAminoAcid("Гистидин", "CAU,CAC,", 'H'));
+			FAmins.push_back(TAminoAcid("Фенилаланин", "UUU,UUC,", 'F'));
+			FAmins.push_back(TAminoAcid("Тирозин", "UAU,UAC,", 'Y'));
+			FAmins.push_back(TAminoAcid("Триптофан", "UGG,", 'W'));
+		};
+		const TAminoAcid& searchAminoAcid(std::string amin)
+		{
+			for (int i = 0; i < FAmins.size(); ++i)
+			{
+				if (FAmins[i].getCodon().find(amin) != std::string::npos)
+				{
+					return FAmins[i];
+				}
+			}
 		}
 	};
 }
